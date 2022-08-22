40d738beddbe8de1c05ed2f9399e8cd79c6ae3e8
maizi80 <624348976@qq.com>
2022-08-22 17:52:37 +0800

    使用migration生成文章表

---
+++ database/migrations/20220822_162618_article.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/beego/beego/v2/client/orm/migration"
+)
+
+// Article_20220822_162618 DO NOT MODIFY
+type Article_20220822_162618 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Article_20220822_162618{}
+	m.Created = "20220822_162618"
+
+	migration.Register("Article_20220822_162618", m)
+}
+
+// Up Run the migrations
+func (m *Article_20220822_162618) Up() {
+	m.CreateTable("article", "innodb", "utf8mb4")
+	m.PriCol("id").SetAuto(true).SetDataType("int(11)").SetUnsigned(true)
+	m.NewCol("title").SetDataType("varchar(50)").SetNullable(false)
+	m.NewCol("introduction").SetDataType("varchar(200) COLLATE utf8_unicode_ci").SetNullable(false)
+	m.NewCol("content").SetDataType("text COLLATE utf8_unicode_ci").SetNullable(false)
+	m.NewCol("image").SetDataType("varchar(32)").SetDefault("''").SetNullable(false)
+	m.NewCol("category_id").SetDataType("tinyint(1)").SetUnsigned(true).SetDefault("1").SetNullable(false)
+	m.NewCol("status").SetDataType("tinyint(1)").SetUnsigned(true).SetDefault("1").SetNullable(false)
+	m.NewCol("sort").SetDataType("tinyint(1)").SetUnsigned(true).SetDefault("1").SetNullable(false)
+	m.NewCol("view_count").SetDataType("int(11)").SetUnsigned(true).SetDefault("0").SetNullable(false)
+	m.NewCol("comment_count").SetDataType("int(11)").SetUnsigned(true).SetDefault("0").SetNullable(false)
+	m.NewCol("like_count").SetDataType("int(11)").SetUnsigned(true).SetDefault("0").SetNullable(false)
+	m.NewCol("created_at").SetDataType("datetime").SetDefault("CURRENT_TIMESTAMP")
+	m.NewCol("updated_at").SetDataType("datetime").SetDefault("CURRENT_TIMESTAMP")
+	m.NewCol("published_at").SetDataType("datetime")
+	m.SQL(m.GetSQL())
+
+}
+
+// Down Reverse the migrations
+func (m *Article_20220822_162618) Down() {
+	m.SQL("DROP TABLE IF EXISTS article")
+}

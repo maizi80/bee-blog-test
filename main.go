1a2dbefcbe480f9b43f8be8a6a85e6615a95222c
maizi80 <624348976@qq.com>
2022-08-22 16:23:17 +0800

    添加连接数据库

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	_ "bee-blog/routers"
+	"database/sql"
 	"fmt"
 	beego "github.com/beego/beego/v2/server/web"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/spf13/viper"
 )
 
@@ -17,5 +19,16 @@ func main() {
 	}
 	appname := viper.Get("appname")
 	fmt.Println(appname)
+	dsn := "root:0987abc123@tcp(192.168.9.204:3306)/bee-blog"
+	//打开数据库链接
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Println("db err:", err)
+		return
+	}
+	//关闭数据库链接
+	defer db.Close()
+	fmt.Println("数据库链接成功")
+
 	beego.Run()
 }

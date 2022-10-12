815b8e1a1d9c2e319219aefd4cd1eac81714638d
maizi80 <624348976@qq.com>
2022-10-12 19:42:15 +0800

    编辑文章

--- controllers/admin.go
+++ controllers/admin.go
@@ -5,8 +5,8 @@ import (
 	"bee-blog/models"
 	"bee-blog/util"
 	"bee-blog/validations"
-	"fmt"
 	"github.com/beego/beego/v2/client/orm"
+	"strconv"
 )
 
 type AdminController struct {
@@ -63,9 +63,80 @@ func (c *AdminController) Post() {
 	}
 	insert, err := orm.NewOrm().Insert(&ao)
 	if err != nil {
-		fmt.Printf(err.Error())
 		commons.Fail(c.Ctx, "添加失败", "", "")
 	}
-	fmt.Println(a, insert)
 	commons.Success(c.Ctx, insert, "添加成功", "")
+}
+
+func (c *AdminController) Edit() {
+	var categorys []models.Category
+	var tags []models.Tag
+	o := orm.NewOrm()
+	o.QueryTable(new(models.Category)).All(&categorys)
+	o.QueryTable(new(models.Tag)).All(&tags)
+
+	aid := c.Ctx.Input.Param(":aid")
+	aidInt, _ := strconv.Atoi(aid)
+	// 验证数据
+	if aidInt == 0 {
+		commons.Fail(c.Ctx, "ID不能为空", "", "")
+	}
+	article := models.Article{Id: uint(aidInt)}
+	err := o.Read(&article)
+	if err == orm.ErrNoRows {
+		c.Abort("404")
+	}
+	c.Data["a"] = article
+	c.Data["categorys"] = categorys
+	c.Data["tags"] = tags
+	c.Data["contains"] = util.ContainsIntInString
+	c.Layout = "admin_layout.tpl"
+	c.TplName = "admin/edit.tpl"
+}
+
+func (c *AdminController) Put() {
+	aid := c.Ctx.Input.Param(":aid")
+	aidInt, _ := strconv.Atoi(aid)
+	cid, _ := c.GetInt("category_id", 0)
+	f, h, err := c.GetFile("image")
+	image := c.GetString("img")
+	if err != nil {
+		image = c.GetString("img")
+	} else {
+		image = "/static/upload/" + h.Filename
+		defer f.Close()
+		c.SaveToFile("image", image)
+	}
+
+	a := validations.ArticleValidate{
+		Title:        c.GetString("title"),
+		CategoryId:   cid,
+		Introduction: c.GetString("introduction"),
+		Content:      c.GetString("content"),
+		Image:        image,
+		Tag:          c.GetStrings("tag"),
+	}
+	commons.Valid(c.Ctx, &a)
+	o := orm.NewOrm()
+	aerr := o.Read(&models.Article{Id: uint(aidInt)})
+	if aerr == orm.ErrNoRows {
+		commons.Fail(c.Ctx, "数据错误", nil, "")
+	}
+
+	// 保存数据
+	ao := models.Article{
+		Id:           uint(aidInt),
+		Title:        a.Title,
+		Introduction: a.Introduction,
+		Content:      a.Content,
+		Image:        a.Image,
+		CategoryId:   uint(a.CategoryId),
+		Tag:          util.Convert(a.Tag),
+	}
+
+	num, err := orm.NewOrm().Update(&ao)
+	if err != nil {
+		commons.Fail(c.Ctx, "更新失败", nil, "")
+	}
+	commons.Success(c.Ctx, num, "更新成功", "")
 }
--- util/util.go
+++ util/util.go
@@ -2,9 +2,21 @@ package util
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func Convert(array interface{}) string {
 	return strings.Replace(strings.Trim(fmt.Sprint(array), "[]"), " ", ",", -1)
+}
+
+func ContainsIntInString(s string, search int64) bool {
+	slice := strings.Split(s, ",")
+	for _, v := range slice {
+		vInt, _ := strconv.Atoi(v)
+		if vInt == int(search) {
+			return true
+		}
+	}
+	return false
 }
